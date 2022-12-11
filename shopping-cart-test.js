f2669213b65f7831a2b7a2e3852cc124cc7c6b3b
Carmen Chow <carmendevelops@gmail.com>
2022-12-11 01:50:50 -0500

    create tests

--- shopping-cart-test.js
+++ shopping-cart-test.js
@@ -1,19 +1,39 @@
 let assert = require('assert');
 let ShoppingCart = require('./shopping-cart');
 
+
 describe('The shopping cart', () => {
   describe('subtotal should', () => {
     it ('be 0 if no items are passed in', () => {
-
-      // Arrange - setting up the code to test a situation
+      // Arrange - set up the code to test a situation
       let shoppingCart = new ShoppingCart();
-    
       // Act
       let subtotal = shoppingCart.subtotal;
-
       // Assert
       assert.equal(subtotal, 0);
+    });
 
+    it ('be the sum of the price * quantity for all items', () => {
+
+      let shoppingCart = new ShoppingCart([
+        {
+          id: 1,
+          quantity: 5,
+          price: 5
+        },
+        {
+          id: 2,
+          quantity: 4,
+          price: 5
+        },
+        {
+          id: 3,
+          quantity: 1,
+          price: 50
+        }
+      ]);
+      assert.equal(shoppingCart.subtotal, 95);
     });
   });
 });
+
